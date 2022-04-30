88fc13092becc2342f51da27a1580d50140d10c0
Kanboo <kanboowang@17.media>
2022-05-01 00:30:15 +0800

    新增首頁

---
+++ src/layouts/Default/Default.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "Default",
+  components: {},
+  props: {},
+
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<template src="./Default.html"></template>
+<style lang="scss" src="./Default.scss" scoped></style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,26 @@
+<script>
+import IMG_IMAGE from "@/assets/images/image.png";
+import IMG_USER from "@/assets/images/user.png";
+import IMG_USER2 from "@/assets/images/user2.png";
+import IMG_USER3 from "@/assets/images/user3.png";
+import IMG_USER4 from "@/assets/images/user4.png";
+import IMG_USER5 from "@/assets/images/user5.png";
+
+export default {
+  name: "Home",
+
+  setup() {
+    return {
+      IMG_IMAGE,
+      IMG_USER,
+      IMG_USER2,
+      IMG_USER3,
+      IMG_USER4,
+      IMG_USER5,
+    };
+  },
+};
+</script>
+
+<template src="./Home.html"></template>
+<style lang="scss" src="./Home.scss" scoped></style>
