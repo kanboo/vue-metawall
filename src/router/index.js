7f3c3d94b5be32d41a1d6e5104c0c22bcb501713
Kanboo <kanboowang@17.media>
2022-05-01 20:51:09 +0800

    調整 router history 模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/Home";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       name: "Home",
