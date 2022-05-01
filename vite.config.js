a8d277d98cf8a8fe3848b8cd2b36346e4abee729
Kanboo <kanboowang@17.media>
2022-05-01 13:56:02 +0800

    更新 vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,31 @@
 import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    base: "./",
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          // eslint-disable-next-line no-undef
+          target: `${process.env.VITE_BACKEND_HOST || "http://localhost:3005"
+            }/`, // 遠端 URL Domain
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });
