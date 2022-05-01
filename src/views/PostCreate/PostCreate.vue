aec398566c664ce531d9ddd5e1e3f9c29dddaea5
Kanboo <kanboowang@17.media>
2022-05-01 16:53:21 +0800

    串接 Post create API

--- src/views/PostCreate/PostCreate.vue
+++ src/views/PostCreate/PostCreate.vue
@@ -1,11 +1,62 @@
 <script>
+import { ref, watch } from "vue";
+import { useRouter } from "vue-router";
+import axios from "axios";
+
 export default {
   name: "PostCreate",
-  components: {},
-  props: {},
 
   setup() {
-    return {};
+    const router = useRouter();
+    const form = ref({
+      content: "",
+      image: null,
+    });
+
+    const displayImage = ref(null);
+    watch(
+      () => form.value.image,
+      (file) => {
+        let reader = new FileReader();
+        reader.onload = (e) => {
+          displayImage.value = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    );
+
+    const onFileChange = (e) => {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+
+      form.value.image = files[0];
+    };
+
+    const submit = async () => {
+      try {
+        const fd = new FormData();
+        fd.append("user", "626e1a1e06f150686b003e03");
+        fd.append("content", form.value.content);
+
+        if (form.value.image) {
+          fd.append("image", form.value.image, form.value.image.name);
+        }
+
+        await axios.post("/api/posts", fd);
+
+        router.push({ name: "Home" });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    return {
+      form,
+      displayImage,
+
+      onFileChange,
+      submit,
+    };
   },
 };
 </script>
