a70e4eebcf494a0f23cd251aa92e690dbd83642e
Kanboo <kanboowang@17.media>
2022-05-05 00:18:35 +0800

    新增 ScreenLoading 元件

---
+++ src/components/ScreenLoading/ScreenLoading.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+  name: "ScreenLoading",
+};
+</script>
+
+<template src="./ScreenLoading.html"></template>
+<style lang="scss" src="./ScreenLoading.scss" scoped></style>
