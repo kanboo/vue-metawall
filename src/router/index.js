fe625182283c61cf9510b61a00c08437fd2b4f39
Kanboo <kanboowang@17.media>
2022-05-07 09:43:23 +0800

    新增 Login 元件

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/views/Home/Home.vue";
+import HomeView from "@/views/Home";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,9 +10,15 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      name: "Login",
+      path: "/login",
+      component: () => import("@/views/Login"),
+      meta: { isExcludeLayout: true },
+    },
+    {
       name: "PostCreate",
       path: "/posts/create",
-      component: () => import("@/views/PostCreate/PostCreate.vue"),
+      component: () => import("@/views/PostCreate"),
     },
   ],
 });
---
+++ src/views/Login/Login.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  name: "Login",
+  components: {},
+  props: {},
+};
+</script>
+
+<template src="./Login.html"></template>
+<style lang="scss" src="./Login.scss" scoped></style>
