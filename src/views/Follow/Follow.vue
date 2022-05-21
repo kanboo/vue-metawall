ce06404c263504c5d7121c123a4d7136ff710fbf
Kanboo <kanboowang@17.media>
2022-05-22 00:34:24 +0800

    新增 Follow 頁面

---
+++ src/views/Follow/Follow.vue
@@ -0,0 +1,60 @@
+<script>
+import { ref, computed } from "vue";
+import { useRouter } from "vue-router";
+
+import axios from "@/plugins/http.js";
+
+import { toggleScreenLoading } from "@/store/screenLoadingStatus";
+
+import SideMenu from "@/components/SideMenu";
+
+const ICON_DEFAULT_USER = "https://api.iconify.design/ri:user-5-line.svg";
+
+export default {
+  name: "Follow",
+  components: { SideMenu },
+
+  setup() {
+    const users = ref([]);
+
+    const normalizedUsers = computed(() => {
+      return users.value.map((user) => {
+        return {
+          ...user,
+          photoDisplay: user?.photo || ICON_DEFAULT_USER,
+        };
+      });
+    });
+
+    const getFollowList = async () => {
+      try {
+        toggleScreenLoading(true);
+
+        const response = await axios.get("/api/v1/user/follow-list");
+        users.value = response.data?.data ?? [];
+      } catch (e) {
+        console.error(e);
+      } finally {
+        toggleScreenLoading(false);
+      }
+    };
+
+    const router = useRouter();
+    const goPersonalPage = (userId) => {
+      router.push({ name: "Personal", params: { personalId: userId } });
+    };
+
+    // INIT ********************************************************************
+    getFollowList();
+    // INIT ********************************************************************
+
+    return {
+      normalizedUsers,
+      goPersonalPage,
+    };
+  },
+};
+</script>
+
+<template src="./Follow.html"></template>
+<style lang="scss" src="./Follow.scss" scoped></style>
