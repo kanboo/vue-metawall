27ca4379cdb5ed286a00e52c6d62b7cd4626f65b
Kanboo <kanboowang@17.media>
2022-06-03 13:17:07 +0800

    新增 Privacy 頁面

---
+++ src/views/Privacy/Privacy.vue
@@ -0,0 +1,17 @@
+<script>
+import { ref, computed } from 'vue'
+
+export default {
+  name: 'Privacy',
+  components: {},
+  props: {},
+
+  setup (props, context) {
+
+    return {}
+  },
+}
+</script>
+
+<template src="./Privacy.html"></template>
+<style lang="scss" src="./Privacy.scss" scoped></style>
